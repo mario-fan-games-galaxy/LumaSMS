3d1c56fe097b9070f8d6b150a26d94e773559eb2
hyliandev <supergoombario@gmail.com>
2018-06-29 18:50:14 -0400

    Abstracted news post views

--- lumasms/views/information.php
+++ lumasms/views/information.php
@@ -1,7 +1,9 @@
 <div class="card">
-	<h2 class="card-header">
-		<?=$title?>
-	</h2>
+	<?php if(!empty($title)): ?>
+		<h2 class="card-header">
+			<?=$title?>
+		</h2>
+	<?php endif; ?>
 	
 	<div class="card-body">
 		<?=$message?>
