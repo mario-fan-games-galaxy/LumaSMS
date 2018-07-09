b1b6882994709e2ac40d9a0b983a45d47872aaec
hyliandev <hyliandev@users.noreply.github.com>
2018-07-09 16:01:41 -0400

    All resource controllers added; CRUD routes easy to add; temp staff page

---
+++ lumasms/controllers/HowtosController.php
@@ -0,0 +1,29 @@
+<?php
+
+class HowtosController extends CRUDController {
+	public function __construct(){
+		$this->commentsType=1;
+		
+		$this->idField='rid';
+		
+		$this->type='Howtos';
+		
+		$this->join=[
+			[
+				'table'=>'res_sounds',
+				'type'=>'LEFT',
+				'pkMine'=>'eid',
+				'pkTheirs'=>'eid',
+			]
+		];
+		
+		$this->where=[
+			[
+				'field'=>'type',
+				'value'=>6
+			]
+		];
+	}
+}
+
+?>
---
+++ lumasms/views/sounds/archive.php
@@ -0,0 +1,41 @@
+<h1 id="page-title">
+	Sounds
+</h1>
+
+
+
+
+
+<div>
+	<strong><?=$total?></strong> results / <strong><?=$pages?></strong> pages
+</div>
+
+
+
+
+
+<?=view('pagination',[
+	'pages'=>$pages,
+	'page'=>$page,
+	'baseUrl'=>'sprites/archive'
+])?>
+
+
+
+
+
+<ul class="list-games"><?php foreach($objects as $object): ?>
+	<li>
+		<?=view('sounds/small',$object->data)?>
+	</li>
+<?php endforeach; ?></ul>
+
+
+
+
+
+<?=view('pagination',[
+	'pages'=>$pages,
+	'page'=>$page,
+	'baseUrl'=>'sprites/archive'
+])?>
